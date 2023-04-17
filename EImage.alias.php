a267aab8bc3f35621d44779d0dc73fa1d72aee64
Aleš Kapica <kapica@fel.cvut.cz>
2023-04-17 12:38:43 +0200

    Add alias and title for the Special:EImagePages
    Change-Id: Iad91933cc54f2ed3afdf935d6162d69757b59a94

---
+++ EImage.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+$specialPageAliases = [];
+/** English (English) */
+$specialPageAliases['en'] = [
+	'EImagePages' => [ 'EImagePages' ],
+];
+
+/** Czech (čeština) */
+$specialPageAliases['cs'] = [
+	'EImagePages' => [ 'Výstřižky' ],
+];
